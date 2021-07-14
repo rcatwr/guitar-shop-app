13fbea47f98f17365e21d4c9e466ddf7bcb4bf7c
unknown <richard.minardi@gmail.com>
2021-07-13 22:36:35 -0700

    updated app to use redux persist

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,45 @@
-import { configureStore } from "@reduxjs/toolkit";
+// https://redux-toolkit.js.org/usage/usage-guide
+import {
+  configureStore,
+  getDefaultMiddleware,
+  combineReducers,
+} from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import orderReducer from "./orderSlice";
 import modalsReducer from "./modalDisplaySlice";
 import searchOrderReducer from "./searchOrdersSlice";
 
-export default configureStore({
-  reducer: {
-    orders: orderReducer,
-    modals: modalsReducer,
-    searchOrders: searchOrderReducer,
-  },
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  orders: orderReducer,
+  modals: modalsReducer,
+  searchOrders: searchOrderReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+
+export const persistor = persistStore(store);
